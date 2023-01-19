9463f021e6849947f81f1574a73775adfdeef217
bgomez8095 <49135820+bgomez8095@users.noreply.github.com>
2023-01-18 23:58:44 -0500

    finish design

---
+++ base-apparel-coming-soon-master/base-apparel-coming-soon-master/script.js
@@ -0,0 +1,21 @@
+const sendBtn = document.querySelector(".submit")
+const errorMsg = document.querySelector(".error")
+const errorIcon = document.querySelector(".error-icon")
+const formInput = document.querySelector(".email-input")
+
+const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const timeoutFunc = setInterval(() => {
+    errorMsg.style.display = "none";
+    errorIcon.style.display = "none";
+    formInput.style.border = "1px solid var(--desaturated-red)";
+}, 5000);
+
+sendBtn.addEventListener("click", () => {
+    let email = formInput.value
+    if (!email.match(emailValidation) || email.length == 0) {
+        errorMsg.style.display = "inline";
+        errorIcon.style.display = "inline";
+        formInput.style.border = "2px solid var(--soft-red)";
+    }
+});
